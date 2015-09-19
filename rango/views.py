e25e2cdbc3e88974525d8908167040e6c7eb53d1
lubart2517 <lubomirvihvatniuk@gmail.com>
2015-09-19 21:50:26 +0300

    Added index html

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("Rango says Hello <br/> <a href='/rango/about'>About</a")
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render(request, 'rango/index.html', context_dict)
 def about(request):
 	return HttpResponse("About Rango <br/> <a href='/rango'>Home page</a")
 # Create your views here.
