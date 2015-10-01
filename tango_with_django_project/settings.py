5ec02409218ed7af144dd250d0779e6816627a77
lubart2517 <lubomirvihvatniuk@gmail.com>
2015-10-01 17:32:59 +0300

    Added media server

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -84,7 +84,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
 TEMPLATE_DIRS = (
 TEMPLATE_PATH,
 )
