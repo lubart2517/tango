84b0a5d24ca0454021465fc60173b813993fb6a2
lubart2517 <lubomirvihvatniuk@gmail.com>
2015-11-08 17:50:06 +0200

    added views and likes in category, modified populate script

--- populate_rango.py
+++ populate_rango.py
@@ -8,7 +8,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python',views=128,likes=64)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
@@ -22,7 +22,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat("Django")
+    django_cat = add_cat("Django",views=64,likes=32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
@@ -36,7 +36,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
 
-    frame_cat = add_cat("Other Frameworks")
+    frame_cat = add_cat("Other Frameworks",views=32,likes=16)
 
     add_page(cat=frame_cat,
         title="Bottle",
@@ -58,9 +58,12 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
-    return c
+def add_cat(name,views,likes):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.views = views
+	c.likes = likes
+	c.save()
+	return c
 
 # Start execution here!
 if __name__ == '__main__':
--- rango/models.py
+++ rango/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
-	
+	views = models.IntegerField(default=0)
+	likes = models.IntegerField(default=0)
 	def __str__(self):
 		return self.name
 class Page(models.Model):
@@ -10,7 +11,6 @@ class Page(models.Model):
 	title = models.CharField(max_length=128)
 	url = models.URLField()
 	views = models.IntegerField(default=0)
-	
 	def __str__(self):
 		return self.title
 
