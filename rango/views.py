b516ca9ece0a0f78729a204b4806973ea046af92
lubart2517 <lubomirvihvatniuk@gmail.com>
2015-11-08 18:57:29 +0200

    added categorys to index page

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 def index(request):
-	context_dict = {'boldmessage': "I am bold "}
-	return render(request, 'rango/index.html', context_dict)
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories':category_list}
+	return render(request,'rango/index.html',context_dict)
 def about(request):
 	return render(request, 'rango/about.html')
 # Create your views here.
