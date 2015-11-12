bcd6f02ecc271bab5b3ffb7553ea5e80574891a5
unknown <lubomirvihvatniuk@gmail.com>
2015-11-12 13:29:13 +0200

    Added slug

---
+++ rango/migrations/0004_category_slug.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0003_auto_20151108_1632'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default='', unique=True),
+            preserve_default=False,
+        ),
+    ]
