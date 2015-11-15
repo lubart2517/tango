0b070b5e86c991c667b8b8c24a88f96e62428b7f
unknown <lubomirvihvatniuk@gmail.com>
2015-11-15 19:39:16 +0200

    Changed main page, added links

--- tango_with_django_project/views.py
+++ tango_with_django_project/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-	return HttpResponse("Home page")
+	return render(request, 'home.html')
 # Create your views here.
