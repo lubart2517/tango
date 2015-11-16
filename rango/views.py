1a7a4799457750a3776a1d40af2b3612341ec879
unknown <lubomirvihvatniuk@gmail.com>
2015-11-16 16:45:56 +0200

    Added form

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models  import Page
+from rango.forms import CategoryForm
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
@@ -22,4 +23,16 @@ def category(request, category_name_slug):
 	except Category.DoesNotExist:
 		pass
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print (form.errors)
+	else:
+		form= CategoryForm()
+	return render(request, 'rango/add_category.html', {'form':form})
 # Create your views here.
