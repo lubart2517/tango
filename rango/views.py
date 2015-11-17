fbed93c5f305c33fb33b42b812c1e34a104cadf5
unknown <lubomirvihvatniuk@gmail.com>
2015-11-17 20:49:14 +0200

    added pages for user to add

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models  import Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
+
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	pages_list = Page.objects.order_by('-views')[:5]
@@ -20,6 +22,7 @@ def category(request, category_name_slug):
 		pages = Page.objects.filter(category=category)
 		context_dict['pages']=pages
 		context_dict['category'] = category
+		context_dict['category_name_slug']=category_name_slug
 	except Category.DoesNotExist:
 		pass
 	return render(request, 'rango/category.html', context_dict)
@@ -35,4 +38,25 @@ def add_category(request):
 	else:
 		form= CategoryForm()
 	return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+		else:
+			print (form.errors)
+	else:
+		form= PageForm()
+	context_dict = {'form':form, 'category':cat}
+	return render(request, 'rango/add_page.html', context_dict)	
 # Create your views here.
