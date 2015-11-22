31810dbd4140915aa333f16cf72d56d681fa638d
unknown <lubomirvihvatniuk@gmail.com>
2015-11-22 21:17:57 +0200

    Added user model

---
+++ rango/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0005_auto_20151119_0831'),
+        ('rango', '0006_auto_20151118_2107'),
+    ]
+
+    operations = [
+    ]
---
+++ rango/migrations/0008_userprofile.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('rango', '0007_merge'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('website', models.URLField(blank=True)),
+                ('picture', models.ImageField(blank=True, upload_to='profile_images')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
