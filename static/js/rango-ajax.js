5c7ade0ba2ae3742d77772bd8e8ff4de3ce51133
lubart2517 <lubomirvihvatniuk@gmail.com>
2016-02-06 23:31:00 +0200

    Fixed search category bug

--- static/js/rango-ajax.js
+++ static/js/rango-ajax.js
@@ -11,7 +11,7 @@ $('#suggestion').keyup(function(){
         var query;
         query = $(this).val();
         $.get('/rango/suggest_category/', {suggestion: query}, function(data){
-         $('#cats').html(data);
+			$('#cats').html(data);
         });
 });
 $('.rango-add').click(function(){
